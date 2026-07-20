db9f3f1fbe7d4b9e5a96db42f00cd78a80e4426e
aymenwacheh <aymenwacheh@gmail.com>
2026-07-20 19:46:51 +0200

    buil +asssets images

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2710060105" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2462061743" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
